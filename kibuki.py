82c62a97fe43e28b508cbf4dd8c2d667355e7b82
Andrew Laidlaw <github.com@gwhite.co.uk>
2022-02-04 15:19:31 +0000

    Update to improve logging information

--- kibuki.py
+++ kibuki.py
@@ -3,10 +3,12 @@
 #Setup
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
 
 INPUT_PIN = 4
 OUTPUT_START_PIN = 9
 OUTPUT_END_PIN = 16
+DELAY = 5
 
 array1 = [9,10,11,12]
 array2 = [13, 14, 15, 16]
@@ -17,27 +19,33 @@ current_array = 1
 
 GPIO.setmode(GPIO.BCM)
 
-print("Setting up outputs")
+now = datetime.now()
+print("\nRunning Kibuki drop software\n============================")
+print(now.strftime("%d/%m/%Y %H:%M:%S"))
+
+print("\nSetting up outputs")
 
 outputs = range(OUTPUT_START_PIN,OUTPUT_END_PIN+1)
 for i in outputs:
     GPIO.setup(i,GPIO.OUT)
     GPIO.output(i,GPIO.HIGH)
-    print("Set pin " + str(i) + " as output")
+    print("    Set pin " + str(i) + " as output")
 
-print("Setting up input")
+print("\nSetting up input")
 
 GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-print("Set pin " + str(INPUT_PIN) + " as input")
+print("    Set pin " + str(INPUT_PIN) + " as input")
+
+print("\n-- Ready to drop some Kibukis --\n")
 
 # Define functions
 
 def trigger(i):
-    print("Triggering " + str(i))
+    print("    Triggering " + str(i))
     GPIO.output(i,GPIO.LOW)
     time.sleep(0.05)
     GPIO.output(i,GPIO.HIGH)
-    print("Done")
+    print("    -- Done --")
 
 def trigger_all():
     i = OUTPUT_START_PIN
@@ -51,26 +59,29 @@ def trigger_array(array):
         trigger(i)
     print("Triggered the full array")
 
-if GPIO.input(INPUT_PIN) == GPIO.HIGH:
-    print("High")
-elif GPIO.input(INPUT_PIN) == GPIO.LOW:
-    print("Low")
-else:
-    print("I don't know")
+# Main code section here, with logic to clean up after exiting
 
-while True:
-    if GPIO.input(INPUT_PIN) == GPIO.HIGH:
-        if current_array == 1:
-            trigger_array(array1)
-        elif current_array == 2:
-            trigger_array(array2)
-        elif current_array == 3:
-            trigger_array(array3)
-        elif current_array == 4:
-            trigger_array(array4)
-        else:
-            print("No more arrays to test")
-            print("Count now up to " + str(current_array))
-        current_array = current_array + 1
-        time.sleep(5)
-        print("Ready to trigger again")
+try:
+    while True:
+        if GPIO.input(INPUT_PIN) == GPIO.HIGH:
+            if current_array == 1:
+                trigger_array(array1)
+            elif current_array == 2:
+                trigger_array(array2)
+            elif current_array == 3:
+                trigger_array(array3)
+            elif current_array == 4:
+                trigger_array(array4)
+            else:
+                print("No more arrays to test")
+                print("    Count now up to " + str(current_array))
+            current_array = current_array + 1
+            print("Waiting for " + str(DELAY) + " seconds...")
+            time.sleep(DELAY)
+            print("Ready to trigger again")
+except KeyboardInterrupt:
+    print("\nExiting after " + str(current_array - 1) + " kibuki drops")
+except:
+    print("Something went wrong and the program exited")
+finally:
+    GPIO.cleanup()
